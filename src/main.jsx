c00b4bc120e20428bd496348f23eb5f013f9fb3e
Tarunksaini <tarunshr145@gmail.com>
2024-10-21 14:49:20 +0530

    update routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,27 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import { Signup, Login } from './components/login-page.jsx';
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }, {
+    path: '/login',
+    element: <Login />
+  }, {
+    path: '/register',
+    element: <Signup />
+  }
+]);
 
-  <App />
-)
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
